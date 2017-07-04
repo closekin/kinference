3e21de359177471f12c99ec79286b9a61b811108
David Lawrence Miller <github@ninepointeightone.net>
2017-07-04 17:10:13 +1000

    first cut at find_FSPs_amongst, verrrrry slow

---
+++ R/find_FSPs_amongst.R
@@ -0,0 +1,98 @@
+#' @importFrom atease @ @<-
+#' @importFrom mvbutils do.on
+#' @export
+"find_FSPs_from_HSPs" <- function( snpg, HSPs) {
+  # For pairs already picked as HSPs, ie PLOD(HSP,UP) > eta: they might be FSPs
+
+  # Don't need full pairwise screening for FSPs (do post hoc on a few hundred
+  # HSPs), hence all in R.
+
+  # HSPs should be M*2 matrix of rows in snpg that are HSPs or FSPs
+
+
+  define_genotypes()
+
+  # Transform to 4way genotypes
+  # based on code in find_duplicates
+  # careful, since "factor level" of AB and OO is different in 4way vs 6way
+  sibg <- just_sibg <- snpg[ c( HSPs),]
+
+  sibg@diplos <- genotypes4_ambig
+  sibg[ just_sibg==AO] <- AAO
+  sibg[ just_sibg==AA] <- AAO
+  sibg[ just_sibg==BO] <- BBO
+  sibg[ just_sibg==BB] <- BBO
+  sibg[ just_sibg==OO] <- OO # need to do OO & AB too, since codes are different in 4way vs 6way
+  sibg[ just_sibg==AB] <- AB
+
+
+  extract.named( snpg@locinfo[ cq( PUP4, LOD4)])
+
+
+  # what is happening here? Is this magick?! Need to parcel up somewhere else.
+  OLOD <- LOD4
+  OPUP <- PUP4
+  mg <- OLOD@mg
+
+  # Can't quite do this with vecless!
+  OLOD[ is.na( OLOD)] <- 0 # set to NA for 4way loci
+  n_loci <- nrow( OPUP)
+  PUP4 <- LOD4 <- array( 0, c( n_loci, 4, 4))
+  for( ig in 1:4) {
+    gjseq <- mg[ , ig]
+    XXi[ l, gj] := OPUP[ l, gj=gjseq] # Shouldn't work with new vecless syntax... but does !?
+    PUP4[ l, {ig}, gj] := XXi[ l, gj]
+    # NPUP[,ig,] <- OPUP[ , mg[,ig]]
+
+    XXi[ l, gj] := OLOD[ l, gj=gjseq]
+    LOD4[ l, {ig}, gj] := XXi[ l, gj]
+  }
+
+  dimnames(PUP4) <- dimnames(LOD4) <- list(NULL, rownames(mg), rownames(mg))
+
+  ## calculate the P[g1, g2 | k shared alleles]
+  # since we save the LOD (for HSP vs UP) and the P[UP] calculate
+  # P[g1 g2 | HSP] from that
+  PHSP4 <- exp( LOD4) * PUP4 # Pr[gg|HSP] <- 0.5 * PUP4 + 0.5 * Pr[gg|kappa=1]
+  P_k0 <- PUP4
+  P_k1 <- 2*PHSP4 - PUP4
+  P_k1[ P_k1 < 0] <- 0 # rounding error
+  # P_k2 <- sqrt(diag(P_k0)) <- we are doing this-ish
+  P_k2 <- 0 * P_k0 # get the shape right
+  P_k2[l, i, i] := sqrt(P_k0[l, i, i])
+
+
+  nsib <- nrow( HSPs)
+  nloci <- ncol(sibg)
+
+  # if only R were zero-indexed...
+  kappa_fsp <- c(1/4, 1/2, 1/4)
+  kappa_hsp <- c(1/2, 1/2, 0)
+
+  p12fsp <- p12hsp <- matrix(NA, nloci, nsib)
+
+  evalq(for(i in 1:nsib){
+    for(l in 1:nloci){
+      # P[g_1l g_2l | FSP]
+      p12fsp[l, i] <- kappa_fsp[1]*P_k0[l, as.character(sibg[i, l]),
+                                           as.character(sibg[i + nsib, l])] +
+                      kappa_fsp[2]*P_k1[l, as.character(sibg[i, l]),
+                                          as.character(sibg[i + nsib, l])] +
+                      kappa_fsp[3]*P_k2[l, as.character(sibg[i, l]),
+                                          as.character(sibg[i + nsib, l])]
+      # P[g_1l g_2l | HSP]
+      p12hsp[l, i] <- kappa_hsp[1]*P_k0[l, as.character(sibg[i, l]),
+                                           as.character(sibg[i + nsib, l])] +
+                     kappa_hsp[2]*P_k1[l, as.character(sibg[i, l]),
+                                          as.character(sibg[i + nsib, l])] +
+                     kappa_hsp[3]*P_k2[l, as.character(sibg[i, l]),
+                                          as.character(sibg[i + nsib, l])]
+    }
+  })
+
+  OD_FH <- p12fsp/p12hsp
+  LOD_FH <- log(OD_FH)
+  PLOD <- colSums(LOD_FH)
+
+  return(PLOD)
+}
