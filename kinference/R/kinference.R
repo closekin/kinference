f86d605a4fb4ac08fdb036f9d8e428a0b19a1f35
SMBaylis <42424739+SMBaylis@users.noreply.github.com>
2024-01-11 18:05:47 +1100

    make ilglk_geno respect 3-way genocalls (kinda)

--- kinference/R/kinference.R
+++ kinference/R/kinference.R
@@ -2340,7 +2340,7 @@ stopifnot(
       warning("snpg$locinfo appears to have been modified after kin_power() was last called. You generally want to run find_HSPs with a locinfo that is up-to-date with itself. Consider re-running kin_power() on your snpg and trying again")
   }
 
-  ## if snpg@diplos is genotypes4_ambig, convert to pseudo 6-way
+  ## if snpg@diplos is genotypes4_ambig, convert whole lot to pseudo 6-way
   if( my.all.equal( snpg@diplos, genotypes4_ambig)) {
     tempsnpg <- snpg
     snpg@diplos <- genotypes6
@@ -3301,7 +3301,160 @@ function(pair_geno, LOD, geno1, geno2, symmo, eta, min_keep_PLOD, keep_n, minbin
 #' ## Need an example!
 #'
 #' @export ilglk_geno
+
 "ilglk_geno" <-
+function(snpg, hist_pars=list(), showPlot = TRUE) {
+  define_genotypes()
+  extract.named( snpg@locinfo[ cq( pbonzer)])
+
+  p0 <- pbonzer[,'O'] + pbonzer[,'C']
+  pA <- pbonzer[,'A']
+  pB <- pbonzer[,'B']
+
+  n_samps <- nrow( snpg)
+  n_loci <- ncol( snpg)
+  minfo_fields <- cq( Our_plate, Our_sample) %that.are.in% names( snpg$info)
+
+  if( suppressWarnings( all( snpg@diplos == genotypes6))) {
+  snpg4 <- snpgeno(
+      NULL,
+      diplos=genotypes4_ambig,
+      info= snpg@info[, minfo_fields],
+      locinfo= snpg@locinfo[, 'Locus', drop=FALSE]
+    )
+  snpg4[ snpg==OO] <- OO
+  snpg4[ snpg==AB] <- AB
+  snpg4[ snpg==AA] <- AAO
+  snpg4[ snpg==AO] <- AAO
+  snpg4[ snpg==BB] <- BBO
+  snpg4[ snpg==BO] <- BBO
+  } else if( suppressWarnings( all(snpg@diplos == genotypes4_ambig))) {
+      snpg4 <- snpg
+  } else { warning("snpg@diplos isn't one of genotypes6 or genotypes4_ambig, so I dunno what to do") }
+
+  pgeno4 <- matrix( 0, n_loci, 4, dimnames=list( NULL, genotypes4_ambig))
+  pgeno4[ , OO] <- sqr( p0)
+  pgeno4[ , AB] <- 2*pA*pB
+  pgeno4[ , AAO] <- 2*pA*p0 + pA*pA
+  pgeno4[ , BBO] <- 2*pB*p0 + pB*pB
+  lpgeno4 <- log( pgeno4) # lambda in doco
+
+  pgeno3 <- matrix( 0, n_loci, 3, dimnames=list( NULL, genotypes3))
+  pgeno3[ , AB] <- 2*pA*pB
+  pgeno3[ , AAO] <- 2*pA*p0 + pA*pA
+  pgeno3[ , BBOO] <- 2*pB*p0 + pB*pB + p0*p0
+  lpgeno3 <- log( pgeno3) # lambda in doco
+
+  # These are slowish in vecless 1.0
+  # ... but less error-prone to write
+  K <- function( tt) {
+      ttp1 <- tt+1
+      KK[ j] := SUM_ %[l]% log( SUM_ %[g]% exp( ttp1[ j] * lpgeno[ l, g]))
+    return( c( KK))
+    }
+
+  dK <- function( tt) {
+      ttp1 <- tt+1
+      etp1l[ j, l, g] := exp( ttp1[ j] * lpgeno[ l, g])
+      num[ j, l] := lpgeno[ l, g] %[g]% etp1l[ j, l, g]
+      denom[ j, l] := SUM_ %[g]% etp1l[ j, l, g]
+      dKK[ j] := SUM_ %[l]% (num[ j, l] / denom[ j, l])
+    return( c( dKK))
+    }
+
+  ddK <- function( tt) {
+      ttp1 <- tt+1
+      etp1l[ j, l, g] := exp( ttp1[ j] * lpgeno[ l, g])
+      num[ j, l] := lpgeno[ l, g] %[g]% etp1l[ j, l, g]
+      denom[ j, l] := SUM_ %[g]% etp1l[ j, l, g]
+      num2[ j, l] := sqr( lpgeno[ l, g]) %[g]% etp1l[ j, l, g]
+      dKK[ j] := SUM_ %[l]% ( num2[ j, l] / denom[ j, l] -  sqr( num[ j, l] / denom[ j, l]))
+    return( c( dKK))
+    }
+
+  quick <- TRUE # used to be param
+  if( quick) {
+    lpgeno <- lpgeno3
+    K3 <- compile_vecless( K( -1))
+    dK3 <- compile_vecless( dK( -1))
+    ddK3 <- compile_vecless( ddK( -1))
+    lpgeno <- lpgeno4
+    K4 <- compile_vecless( K( -1))
+    dK4 <- compile_vecless( dK( -1))
+    ddK4 <- compile_vecless( ddK( -1))
+  }
+
+  if( FALSE) { # Checks: do manually in mtrace
+    ntest <- 1000
+    Ktest <- function( tt) { # scalar
+      Ksim <- meansim <- rep( 0, ntest)
+      for( l in 1:n_loci) {
+        # Can't directly sample from genotypes4_ambig since can't matrix-subscript mixed int and char
+        genos <- rsample( ntest, seq_along( genotypes4_ambig), prob=pgeno[l,], replace=TRUE)
+        lp <- lpgeno[ cbind( l, genos)]
+        Ksim <- Ksim + ( tt * lp) # actually log( exp( t*lp))
+        meansim <- meansim + lp # though see below for better way to check!
+      }
+      returnList( Ksim, meansim)
+    }
+
+    dK( 0)
+    sum( pgeno * lpgeno) # should be the same
+  }
+
+  if(any( snpg@locinfo$useN == 3)) {
+      ilglk3 <- indiv_lglk_geno(
+      lpgeno= lpgeno3[ snpg@locinfo$useN == 3,,  drop = FALSE],
+      geno= snpg4[ , snpg@locinfo$useN == 3, drop = FALSE]) ## calc for all individuals, at loci where useN == 3
+  }
+  if( any( snpg@locinfo$useN > 3)) {
+      ilglk4 <- indiv_lglk_geno(
+      lpgeno= lpgeno4[ snpg@locinfo$useN > 3,, drop  =FALSE],
+      geno= snpg4[ , snpg@locinfo$useN > 3, drop = FALSE])
+  }
+  if( !exists('ilglk3')) { ilglk3 <- rep(0, nrow(snpg)) }
+  if( !exists('ilglk4')) { ilglk4 <- rep(0, nrow(snpg)) }
+
+  ilglk <- rowSums(cbind( ilglk3, ilglk4), na.rm = TRUE)
+
+  if( FALSE) { # "manual" check on calcs
+    lp <- lpgeno[ cbind( rep( 1 %upto% n_loci, n_samps), snpg)]
+    dim( lp) <- c( n_loci, n_samps)
+    ilglk_manual <- colSums( lp)
+  }
+
+  ## note that null density assumes 4-way
+  dens_SPA <- renorm_SPA( K4, dK4, ddK4, return='func', already_vectorized=TRUE)
+
+  if( isFALSE( hist_pars)) {
+    showPlot <- FALSE
+  } else {
+    hist_pars <- add_list_defaults( hist_pars,
+        main   = 'Geno lglk by specimen',
+        xlim   = range( ilglk),
+        col    = "grey",
+        border = NA,
+        xlab   = '',
+        nclass = 50)
+  }
+
+  if( showPlot) {
+      lv <- do.call( 'hist', c( list( x=ilglk), hist_pars))
+
+      # some dens_SPA can fail -- do lapply and then weed out baddies
+      mids_SPA <- lapply(lv$mids, function(x) try(dens_SPA( x), silent=TRUE))
+      good_ind <- unlist(lapply(mids_SPA, class) != "try-error")
+      mids_SPA <- unlist(mids_SPA[good_ind])
+      # plot predicted density. Slowish with vecless 1.0
+      lines( lv$mids[good_ind], diff( lv$breaks)[good_ind] * mids_SPA * n_samps,
+            col='green')
+  }
+
+return( ilglk)
+}
+
+
+"OLD_ilglk_geno" <-
 function(snpg, hist_pars=list(), showPlot = TRUE) {
   define_genotypes()
   extract.named( snpg@locinfo[ cq( pbonzer)])
@@ -3337,7 +3490,6 @@ function(snpg, hist_pars=list(), showPlot = TRUE) {
   pgeno[ , AAO] <- 2*pA*p0 + pA*pA
   pgeno[ , BBO] <- 2*pB*p0 + pB*pB
   lpgeno <- log( pgeno) # lambda in doco
-
 
   # These are slowish in vecless 1.0
   # ... but less error-prone to write
